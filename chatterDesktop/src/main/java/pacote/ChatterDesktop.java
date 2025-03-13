91faeb65dc3f7fe863a9068cc6606b4c0a3457fd
KemilyRezende <102739889+KemilyRezende@users.noreply.github.com>
2025-03-12 23:11:52 -0300

    Add files via upload

---
+++ chatterDesktop/src/main/java/pacote/ChatterDesktop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pacote;
+
+/**
+ *
+ * @author kemily
+ */
+public class ChatterDesktop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
